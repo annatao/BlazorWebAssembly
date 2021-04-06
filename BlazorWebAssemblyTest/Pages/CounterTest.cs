8b69f900133e3d7b5be2af3c13d777a37e6b78ff
annatao <40440137+annatao@users.noreply.github.com>
2021-04-06 11:45:11 +0200

    Add unit test project
    - add bunit.web and bunit.xunit
    - add unit test for counter razor omponent

---
+++ BlazorWebAssemblyTest/Pages/CounterTest.cs
@@ -0,0 +1,32 @@
+﻿using BlazorWebAssembly.Pages;
+using Bunit;
+using System;
+using Xunit;
+
+namespace BlazorWebAssemblyTest
+{
+    public class CounterTest
+    {
+        [Fact]
+        public void CountDisplayShouldBeUpdatedWhenButtonIsClicked()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var component = ctx.RenderComponent<Counter>();
+
+            Random rd = new();
+            int rand_num = rd.Next(1, 200);
+
+            // Act
+            for (var j = 0; j<rand_num; j++)
+            {
+                component.Find("button").Click();
+            }
+
+            // Assert
+            var result = $"<h1>Counter</h1><p>Current count: {rand_num}</p><button class='btn btn-primary'>Click me</button>";
+            component.MarkupMatches(result);
+        }
+
+    }
+}
