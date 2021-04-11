df0edf164d7d5d0dc4c2b1019f5ae6f777151033
annatao <40440137+annatao@users.noreply.github.com>
2021-04-11 13:33:39 +0200

    add unit test for FetchWeather component

---
+++ BlazorWebAssemblyTest/MockConfigurationHelpers.cs
@@ -0,0 +1,18 @@
+﻿using Bunit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorWebAssemblyTest
+{
+    public static class MockConfigurationHelpers
+    {
+        public static IConfiguration AddMockConfiguration(this TestServiceProvider services)
+        {
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.test.json")
+               .Build();
+            services.AddSingleton<IConfiguration>(config);
+            return config;
+        }
+    }
+}
---
+++ BlazorWebAssemblyTest/Pages/FetchWeatherTest.cs
@@ -0,0 +1,59 @@
+﻿using BlazorWebAssembly.Pages;
+using Bunit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace BlazorWebAssemblyTest.Pages
+{
+    public class FetchWeatherTest
+    {
+        [Fact]
+        public void TestFetchWeather_NullCurrentWeather()
+        {
+            // Arrange
+            using var ctx = new Bunit.TestContext();
+            ctx.Services.AddMockConfiguration();
+            ctx.Services.AddMockHttpClient();
+
+            // Act
+            var component = ctx.RenderComponent<FetchWeather>();
+
+            // Assert
+            var result = @"<p><em>Loading...</em></p>";
+            component.MarkupMatches(result);
+        }
+
+        [Fact]
+        public void TestFetchWeather_NotNullCurrentWeather()
+        {
+            // Arrange
+            using var ctx = new Bunit.TestContext();
+            var mockConfiguration = ctx.Services.AddMockConfiguration();
+
+            var mockClient = ctx.Services.AddMockHttpClient();
+            var mockAPI = String.Format(mockConfiguration["OpenWeatherMapAPIUrl"], "Stockholm");
+            var mockCurrentWeather = new Mock<FetchWeather.CurrentWeather>().SetupAllProperties().Object;
+            mockCurrentWeather.Main = new Mock<FetchWeather.Main>().SetupAllProperties().Object;
+            mockCurrentWeather.Coord = new Mock<FetchWeather.Coord>().SetupAllProperties().Object;
+            mockCurrentWeather.Weather = new[] { new Mock<FetchWeather.Weather>().SetupAllProperties().Object };
+            mockCurrentWeather.Wind = new Mock<FetchWeather.Wind>().SetupAllProperties().Object;
+            mockCurrentWeather.Clouds = new Mock<FetchWeather.Clouds>().SetupAllProperties().Object;
+            mockCurrentWeather.Sys = new Mock<FetchWeather.Sys>().SetupAllProperties().Object;
+            mockClient.When(mockAPI).RespondJson(mockCurrentWeather);
+
+            // Act
+            var component = ctx.RenderComponent<FetchWeather>();
+
+            // Assert
+            component.WaitForState(() => component.FindAll(".table").Count > 0); // blocks until the predicate passes or the timeout is passed (default 1 sec).
+            component.WaitForAssertion(() => NUnit.Framework.Assert.AreEqual(1, component.FindAll(".table").Count)); 
+        }
+    }
+}
